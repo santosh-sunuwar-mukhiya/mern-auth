9dd64be25f2fce5bdcb8dadeb37ff26d9d291df9
santoshsunuwarmukhiya <santoshsunuwarmukhiya@gmail.com>
2026-08-26 15:00:56 +0545

    completed the models, controller, utils, working on routes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express"
-import connectDB from "./config/db.js"
 import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from 'cookie-parser';
+
+import connectDB from "./config/db.js"
 
 dotenv.config()
 const PORT = process.env.PORT || 8000
 
 const app = express()
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}))
 
 app.use('/', (req, res) => {
     res.send('Hello World From Express!')
