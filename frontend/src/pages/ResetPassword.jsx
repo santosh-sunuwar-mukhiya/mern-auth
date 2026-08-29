277775ac320f86eaa3b6073d06290471db0b8308
santoshsunuwarmukhiya <santoshsunuwarmukhiya@gmail.com>
2026-08-29 15:28:03 +0545

    reset password working ..

--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,7 +1,12 @@
+import {assets} from "../assets/assets.js";
+import React from "react";
+import {useNavigate} from "react-router-dom";
+
 export default function ResetPassword() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">This is ResetPassword Page.</h1>
-    </>
-  )
+    const navigate = useNavigate();
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
+            <img onClick={()=> navigate('/')} src={assets.logo} className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' alt={'logo'}/>
+        </div>
+    )
 }
