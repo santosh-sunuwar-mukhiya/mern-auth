0d1525fe9c0a41c7d22e37692af27b4dd04eb6ed
santoshsunuwarmukhiya <santoshsunuwarmukhiya@gmail.com>
2026-08-29 14:28:19 +0545

    email verify and otp featured

--- frontend/src/pages/EmailVerify.jsx
+++ frontend/src/pages/EmailVerify.jsx
@@ -1,35 +1,70 @@
 import {assets} from "../assets/assets.js";
 import {useNavigate} from "react-router-dom";
-import React from "react";
+import React, {useContext} from "react";
+import {AppContent} from "../context/AppContext.jsx";
+import axios from "axios";
+import {toast} from "react-toastify";
 
 export default function EmailVerify() {
+    axios.defaults.withCredentials = true;
+    const {backendUrl, isLoggedIn, userData, getUserData} = useContext(AppContent);
     const inputRefs = React.useRef([]);
     const navigate = useNavigate();
-    const handleInput = async (e, i) => {
+
+    const handleInput = (e, i) => {
         if(e.target.value.length > 0 && i < inputRefs.current.length - 1) {
             inputRefs.current[i + 1].focus()
         }
     }
 
     const handleKeyDown = (e, i) => {
-        if (e.key === 'Backspace' || e.target.value === '' && i > 0) {
-            inputRefs.current[i - 1].focus()
+        if (e.key === "Backspace" && e.target.value === "" && i > 0) {
+            inputRefs.current[i - 1].focus();
         }
-    }
+    };
 
     const handlePaste = (e) => {
-        const paste = e.clipboardData.getData("text/plain");
-        const pasteArray = paste.split('');
-        pasteArray.forEach((char, i) => {
-            if(inputRefs.current[i]){
+        e.preventDefault();
+
+        const paste = e.clipboardData
+            .getData("text")
+            .slice(0, 6);
+
+        paste.split("").forEach((char, i) => {
+            if (inputRefs.current[i]) {
                 inputRefs.current[i].value = char;
             }
-        })
+        });
+
+        if (inputRefs.current[paste.length - 1]) {
+            inputRefs.current[paste.length - 1].focus();
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        try{
+            e.preventDefault();
+            const otpArray = inputRefs.current.map(e=> e.value);
+            const otp = otpArray.join('');
+
+            const {data} = await axios.post(`${backendUrl}/api/auth/verify-account`,{otp});
+
+            if(data.success){
+                toast.success(data.message);
+                getUserData();
+                navigate('/');
+            }else{
+                toast.error(data.message);
+            }
+
+        }catch(err){
+            toast.error(err.message)
+        }
     }
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
       <img onClick={()=> navigate('/')} src={assets.logo} alt={'logo'} className={'absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'} />
-        <form className={'bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'}>
+        <form className={'bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'} onSubmit={handleSubmit}>
             <h1 className={'text-white text-2xl font-semibold text-center mb-4'}>Email Verify OTP</h1>
             <p className={'text-center mb-6 text-indigo-300'}>Enter the 6 digit code sent to your email id.</p>
             <div className={'flex justify-between mb-8'} onPaste={handlePaste}>
@@ -37,7 +72,7 @@ export default function EmailVerify() {
                     <input type={'text'} key={i} maxLength={1} required
                            className={'w-12 h-12 bg-[#333A53] text-white text-center text-xl rounded-md'}
                            ref={e => inputRefs.current[i] = e}
-                           onInput={(e)=> handleInput(e, i)}
+                           onChange={(e)=> handleInput(e, i)}
                            onKeyDown={(e)=>handleKeyDown(e, i)}
                     />
                 ))}
